907febc9ddef7dfb167313578b1544ce1f795dc7
Ashish Kumar <ashishkumar901336@gmail.com>
2024-10-13 20:54:59 +0530

    added text input

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
@@ -22,26 +23,37 @@ func main() {
 // Model: app state
 type Model struct {
 	title string
+
+	textinput textinput.Model
 }
 
 // NewModel: initial model
 func NewModel() Model {
+	ti := textinput.New()
+	ti.Placeholder = "Enter search term"
+	ti.Focus()
+
 	return Model{
-		title: "hello world",
+		title:     "hello world",
+		textinput: ti,
 	}
 }
 
 // Init: event loop
 func (m Model) Init() tea.Cmd {
-	return nil
+	return textinput.Blink
 }
 
 // Update: handle mssgs
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return m, nil
+	var cmd tea.Cmd
+	m.textinput, cmd = m.textinput.Update(msg)
+
+	return m, cmd
 }
 
 // View: return a string based on the state of our model
 func (m Model) View() string {
-	return m.title
+	s := m.textinput.View()
+	return s
 }
